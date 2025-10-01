cf04cc47ca560fa4668cf1a22ef5a10eba5a662b
Guillaume Carraux <guillaumecarr1@gmail.com>
2025-10-01 07:58:31 +0000

    first draft of auto-annotation

--- compiler/src/dotty/tools/dotc/Compiler.scala
+++ compiler/src/dotty/tools/dotc/Compiler.scala
@@ -143,7 +143,8 @@ class Compiler {
          new sjs.JUnitBootstrappers, // Generate JUnit-specific bootstrapper classes for Scala.js (not enabled by default)
          new CollectEntryPoints,     // Collect all entry points and save them in the context
          new CollectSuperCalls,      // Find classes that are called with super
-         new RepeatableAnnotations) :: // Aggregate repeatable annotations
+         new RepeatableAnnotations,  // Aggregate repeatable annotations
+         new NullifyAtLastUse) ::    // Tag all last uses of variables to add cleaning bytecode
     Nil
 
   /** Generate the output of the compilation */
